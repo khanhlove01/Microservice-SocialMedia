dbee0c8da3317b942e9b42fdc541b5587ee80422
Tran Minh Khanh <kidopro03@gmail.com>
2025-04-11 23:39:44 +0700

    Search Service

---
+++ search-service/src/server.js
@@ -0,0 +1,56 @@
+require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const Redis = require("ioredis");
+const cors = require("cors");   
+const helmet = require("helmet");
+const errorHandler = require("./middleware/errorHandler");
+const logger = require("./utils/logger");
+const {connectToRabbitMQ,consumeEvent} = require('./utils/rabbitmq');
+const searchRoutes = require('./routes/search-routes');
+const { handlePostCreated, handlePostDeleted } = require('./eventHandlers/search-event-handler');
+
+const app = express()
+const PORT = process.env.PORT || 3004
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => logger.info("Connected to DB"))
+  .catch((e) => logger.error("Mongo Connection error", e));
+
+const redisClient = new Redis(process.env.REDIS_URL);
+
+//midlleware
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.info(`Request body, ${req.body}`);
+  next();
+});
+
+app.use('/api/search',searchRoutes)
+
+app.use(errorHandler)
+
+async function startServer(){
+    try {
+        await connectToRabbitMQ();
+
+        //consume the events / subscribe to the events
+        await consumeEvent('post.created',handlePostCreated)
+        await consumeEvent('post.deleted',handlePostDeleted)
+
+        app.listen(PORT,() => {
+            logger.info("Search service is running on port " + PORT)
+        })
+        
+    } catch (error) {
+        logger.error(e, 'Failed to start search service');
+        process.exit(1)
+    }
+}
+
+startServer();
